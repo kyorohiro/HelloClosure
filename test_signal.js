2902cc64832aa239bcf58b3ecd60104678fc44c2
kyorohiro <kyorohiro@gmail.com>
2014-04-29 21:19:21 +0900

    ..

--- test_signal.js
+++
@@ -1,7 +0,0 @@
-require('./closure-library/closure/goog/bootstrap/nodejs');
-require('./closure-library/kyoro/hetima/deps.js');
-goog.require('hetima.signal.SignalServer');
-
-var server = new hetima.signal.SignalServer();
-server.startServer("127.0.0.1", 8080);
-server.startWSServer();
